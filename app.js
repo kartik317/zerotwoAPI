ed77db85d298f2fd97cb40e67badba257a954639
kartik317 <tocodewith@gmail.com>
2025-04-08 14:27:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");  // Allow all origins
+  res.header("Access-Control-Allow-Origin", "http://localhost:8000");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
