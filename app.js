4398d6258284a3612b7b5ca2cd224f73fcb5e163
kartik317 <tocodewith@gmail.com>
2025-04-08 14:21:24 +0530

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const cors = require('cors');
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");  // Allow all origins
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+
 // Sample image data (replace with your actual URLs or paths)
 const images = [
   {
